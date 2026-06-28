5ce79977d04cedd8de603f82393a53e1576e5a06
Alene-Ayenew <aleneayenew14@gmail.com>
2026-06-28 10:36:10 +0300

    starting my project after rest(break)

---
+++ frontend/src/Components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./Header.css"
+function Header() {
+  return (
+    <div className="header">
+        <div className="header-content">
+            <h2>Order Your Favorite Food Here</h2>
+            <p>choose from a diverse menu featuring a delectable array of dishes crafted
+                 with the finest ingredients and satisfy 
+                 your craving and elevate your dining experience, one delicious meal at a time  </p>
+                 <button>View Menu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
