c1cae6474ba22ec79281d8bfa682d3b0f4a19b58
Alene-Ayenew <aleneayenew14@gmail.com>
2026-07-26 21:42:05 +0300

    implementing to display food items(lists) displaying foods

--- frontend/src/context/storeContext.jsx
+++ frontend/src/context/storeContext.jsx
@@ -2,7 +2,9 @@ import { createContext, useContext } from "react";
 import {food_list} from "../assets/assets"
 export const StoreContext = createContext("null");
 const StoreContextProvider = (props) => {
-  const contextValue = {};
+  const contextValue = {
+    food_list
+  };
   return (
     <StoreContext.Provider value={contextValue}>
       {props.children}
