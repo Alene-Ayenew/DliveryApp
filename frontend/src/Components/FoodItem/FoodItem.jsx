25efdc356da5d1f2dcafb1d3f995e7307203a05b
Alene-Ayenew <aleneayenew14@gmail.com>
2026-07-28 21:48:00 +0300

    impelementing css for FoodDisplay

--- frontend/src/Components/FoodItem/FoodItem.jsx
+++ frontend/src/Components/FoodItem/FoodItem.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import "./FoodItem.css";
 import { assets } from "../../assets/assets";
-function FoodItem({id,name,description,price,image}) {
+function FoodItem({ id, name, description, price, image }) {
   return (
     <div className="food-item">
       <div className="food-item-image-container">
         <img className="food-item-image" src={image} alt="" />
       </div>
       <div className="food-item-info">
-        <div className="food-item name-rating">
+        <div className="food-item-name-rating">
           <p>{name}</p>
           <img src={assets.rating_starts} alt="" />
         </div>
