2dc63ced6f6a763b269a2614e4415d722e47a0c3
Alene-Ayenew <aleneayenew14@gmail.com>
2026-07-28 22:41:39 +0300

    implementing counter for FoodItem (by using add and remove icons===> when add icon is clicked it counts prev+1 and so on)

--- frontend/src/Components/FoodItem/FoodItem.jsx
+++ frontend/src/Components/FoodItem/FoodItem.jsx
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import "./FoodItem.css";
 import { assets } from "../../assets/assets";
 function FoodItem({ id, name, description, price, image }) {
+  const [itemCount, setItemCount] = useState(0);
   return (
     <div className="food-item">
       <div className="food-item-image-container">
         <img className="food-item-image" src={image} alt="" />
+        {!itemCount ? (
+          <img className="add" onClick={()=>setItemCount(prev=>prev+1)} src={assets.add_icon_white} alt="" />
+        ) : (
+          <div className="food-item-counter">
+            <img src={assets.remove_icon_red} onClick={()=>setItemCount(prev=>prev-1)} alt="" />
+            <p>{itemCount}</p>
+            <img src={assets.add_icon_green} onClick={()=>setItemCount(prev=>prev+1)} alt="" />
+          </div>
+        )}
       </div>
       <div className="food-item-info">
         <div className="food-item-name-rating">
