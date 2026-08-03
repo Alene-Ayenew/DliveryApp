943fdccce436fdeed24cb05b41abdd5046559bff
Alene-Ayenew <aleneayenew14@gmail.com>
2026-08-03 10:49:06 +0300

    implementing cart page

--- frontend/src/Pages/Cart/Cart.jsx
+++ frontend/src/Pages/Cart/Cart.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import "./Cart.css"
+import { StoreContext } from '../../context/storeContext'
 function Cart() {
+  const {cartItems,removeFromCart,food_list}=useContext(StoreContext);
   return (
-    <div>Cart</div>
+    <div className='cart'>
+      <div className="cart-items">
+        <div className="cart-items-title">
+          <p>Items</p>
+          <p>Title</p>
+          <p>Price</p>
+          <p>Quantity</p>
+          <p>Total</p>
+          <p>Remove</p>
+        </div>
+        <br />
+        <hr />
+        {food_list.map((item,index)=>{
+          if(cartItems[item._id]>0){
+            return(
+              <div className="cart-items-title cart-items-item">
+                <img src={item.image} alt="" />
+                <p>{item.name}</p>
+                <p>{item.price}</p>
+                <p>{cartItems[item._id]}</p>
+                <p>{item.price*cartItems[item._id]}</p>
+                <p>X</p>
+              </div>
+            )
+          }
+        })}
+      </div>
+      
+
+    </div>
   )
 }
 
