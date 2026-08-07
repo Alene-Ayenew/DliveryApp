3e43b27acc946fc42f954ee8cbb1498b146cd214
Alene-Ayenew <aleneayenew14@gmail.com>
2026-08-07 13:43:25 +0300

    implementing placeOrder page

--- frontend/src/Pages/Cart/Cart.jsx
+++ frontend/src/Pages/Cart/Cart.jsx
@@ -1,8 +1,10 @@
 import React, { useContext } from "react";
 import "./Cart.css";
 import { StoreContext } from "../../context/storeContext";
+import { useNavigate } from "react-router-dom";
 function Cart() {
   const { cartItems, removeFromCart, food_list,getTotalCartAmount} = useContext(StoreContext);
+  const navigate=useNavigate()
   return (
     <div className="cart">
       <div className="cart-items">
@@ -54,7 +56,7 @@ function Cart() {
               <b>${getTotalCartAmount()+2}</b>
             </div>
           </div>
-          <button>PROCEED TO CHECKOUT</button>
+          <button onClick={()=>navigate("/order")} >PROCEED TO CHECKOUT</button>
         </div>
         <div className="cart-promocode">
           <div>
